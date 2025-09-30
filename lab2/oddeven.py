6aaac5ba0465f7c9c668295b38ef943ef38c8a6a
dhruv <dhruva8vernekar@gmail.com>
2025-09-30 08:39:24 +0530

    save

---
+++ lab2/oddeven.py
@@ -0,0 +1,8 @@
+# odd or even number check
+num =int(input("enter a number"))
+if num % 2 == 0:
+    print(f"(num) is a even number")
+else:
+    print(f"(num) is a odd number")
+    print("this program checks weather a number is even or odd")
+    
