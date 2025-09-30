37d3cf896d9f971af386585f85e1fc6e10d5ba54
dhruv <dhruva8vernekar@gmail.com>
2025-09-30 11:38:52 +0530

    saving file

---
+++ prg3.py
@@ -0,0 +1,14 @@
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+print("Before swapping:")
+print("Value of num1:", num1)
+print("Value of num2:", num2)
+
+temp = num1
+num1 = num2
+num2 = temp
+
+print("After swapping:")
+print("Value of num1:", num1)
+print("Value of num2:",num2)
